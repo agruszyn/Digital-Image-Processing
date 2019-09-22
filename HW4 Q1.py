3135378f9b2ee58145031b7e6ccf4920c8237dc3
Tony <agruszyn@gmail.com>
2019-09-22 14:36:45 +0200

    Completed Question 2

--- HW4 Q1.py
+++ HW4 Q1.py
@@ -1,7 +1,7 @@
 import dippykit as dip
 import numpy as np
 
-A = np.array([[90, 93, 156],[93, 156, 155],[156, 155, 155]])
+A = np.array([[90.0, 93.0, 156.0],[93.0, 156.0, 155.0],[156.0, 155.0, 155.0]])
 I = np.array([[0.5,0.0],[0.0,0.5]])
 
 Ahat = dip.resample(A, I, interpolation='bilinear')
--- HW4_Q2.py
+++ HW4_Q2.py
@@ -2,49 +2,69 @@ import dippykit as dip
 import numpy as np
 
 
+# The Function for Q2 is defined here
+# Takes in the image and parameters (rowise scaling, columnwise scaling, and rotation)
+# The function is overloaded so that inputing the original size will do the inverse transform
 def transform(img, rows, cols, rot, size=None):
-    s = np.sin(rot*180/np.pi)
-    c = np.cos(rot*180/np.pi)
-    r = np.array([[c, -s], [s, c]])
-    m = np.array([[(1/rows), 0], [0, (1/cols)]])
+
+    s = np.sin(rot*180/np.pi)                       # Sin(theta)
+    c = np.cos(rot*180/np.pi)                       # Cos(theta)
+    r = np.array([[c, -s], [s, c]])                 # Rotation Matrix
+    m = np.array([[(1/rows), 0], [0, (1/cols)]])    # Sampling matrix
+
+    # If doing a regular transformation, resize the image then rotate
     if size is None:
         img = dip.resample(img, m, interpolation='bicubic')
         return dip.resample(img, r, interpolation='bicubic')
+    # If doing an inverse transformation, rotate image then resize
     else:
         img = dip.resample(img, r, interpolation='bicubic')
-        return dip.resample(img, m, crop = True, crop_size=size, interpolation='bicubic')
+        return dip.resample(img, m, crop=True, crop_size=size, interpolation='bicubic')
 
 
+# Read in the image
 X = dip.im_read('images/cameraman.tif')
 X = dip.im_to_float(X)
+
+# Transform image
 Xnew = transform(X, 2.5, 1.7, 27.5)
+
+# Inverse transformation
 Xre = transform(Xnew, 1/2.5, 1/1.7, -27.5, np.shape(X))
+
+# Difference between original and new
 Xdif = X-Xre
 
+# Create a subplot, display the original image
 dip.subplot(2, 2, 1)
-dip.xlabel('x')
-dip.ylabel('y')
-dip.title('Original Image')
+dip.xlabel('x', fontsize=20)
+dip.ylabel('y', fontsize=20)
+dip.title('Original Image', fontsize=20)
 dip.imshow(X, cmap='gray')
 
+# Display transformed image
 dip.subplot(2, 2, 2)
-dip.xlabel('x')
-dip.ylabel('y')
-dip.title('transformed and rotated')
+dip.xlabel('x', fontsize=20)
+dip.ylabel('y', fontsize=20)
+dip.title('b: transformed and rotated', fontsize=20)
 dip.imshow(Xnew, cmap='gray')
 
+# Display recreated image
 dip.subplot(2, 2, 3)
-dip.xlabel('x')
-dip.ylabel('y')
-dip.title('transformed back')
+dip.xlabel('x', fontsize=20)
+dip.ylabel('y', fontsize=20)
+dip.title('c: transformed back', fontsize=20)
 dip.imshow(Xre, cmap='gray')
 
+# Display the difference between images
 dip.subplot(2, 2, 4)
-dip.xlabel('x')
-dip.ylabel('y')
-dip.title('difference between images')
+dip.xlabel('x', fontsize=20)
+dip.ylabel('y', fontsize=20)
+dip.title('d: difference between images', fontsize=20)
 dip.imshow(Xdif, cmap='gray')
-print(X)
+
+# Calculate PSNR
 print(dip.PSNR(X, Xre, 1))
 
+# Show plots and finish
 dip.show()
